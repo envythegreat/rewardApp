38e72d61939a9283e91af3ad3f4e47aab30be013
Alouani Mohamed Habib <greed.envyme@gmail.com>
2020-09-17 13:43:41 -0400

    home working on displaying ads

--- src/tabs/Home.tsx
+++ src/tabs/Home.tsx
@@ -1,46 +1,27 @@
-import { Animated, Easing, Text } from 'react-native';
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
-// import Container from '../component/Container';
-import { Allshapes,} from '../component/icons/Shapes';
-// import HeaderAuth from '../component/HeaderAuth';
-// import HeaderAuth from '../component/HeaderAuth';
-// import SplashSVG from '../component/icons/splash';
+import {StyleSheet } from 'react-native';
+import { Container, Header, Left,  Right, Icon,  Text, Badge, View } from 'native-base';
+import HeaderSVG from '../component/icons/HeaderSVG'
 
 interface HomeProps {}
 const Home = (_props: HomeProps) => {
-  const spinValue = new Animated.Value(0)
 
-// First set up animation 
-Animated.loop(
-  Animated.timing(
-    spinValue,
-    {
-     toValue: 1,
-     duration: 2000,
-     easing: Easing.linear,
-     useNativeDriver: true
-    }
-  )
- ).start();
-
-// Second interpolate beginning and end values (in this case 0 and 1)
-const spin = spinValue.interpolate({
-  inputRange: [0, 1],
-  outputRange: ['0deg', '360deg']
-})
   return (
-    <View style={styles.Bigcontainer}>
-      <Animated.View style={[
-        {transform: [{rotate: spin}] },
-      ]}>
-        <Allshapes />
-      </Animated.View>
-      <View style={styles.TextM}>
-        <Text style={styles.txt}>Nova Reward</Text>
-      </View>
-    </View>
+    <Container>
+      <Header transparent>
+        <Left>
+          <View>
+            <HeaderSVG />
+          </View>
+        </Left>
+        <Right>
+          <Badge style={{ backgroundColor: 'lightblue', flexDirection: 'row'}}>
+            <Text style={{ color: 'white' }}>1866</Text>
+            <Text style={{ color: 'white' }}>Point</Text>
+          </Badge>
+        </Right>
+      </Header>
+    </Container>
   );
 };
 
@@ -53,13 +34,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  TextM:{
-    position: 'absolute',
-    bottom: 100,
-    justifyContent: 'center'
-  },
-  txt:{
-    fontSize: 40,
-    fontFamily: 'BalooTamma2'
-  }
 });
