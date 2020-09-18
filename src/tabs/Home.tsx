1c79b44a3fe07182a90ef72bec9f32fe70f48b13
Alouani Mohamed Habib <greed.envyme@gmail.com>
2020-09-18 10:30:41 -0400

    showing ads

--- src/tabs/Home.tsx
+++ src/tabs/Home.tsx
@@ -1,28 +1,66 @@
-import React from 'react';
-import {StyleSheet } from 'react-native';
-import { Container, Header, Left,  Right, Icon,  Text, Badge, View } from 'native-base';
-import HeaderSVG from '../component/icons/HeaderSVG'
+import React, { Component } from 'react';
+import { StyleSheet, Platform} from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Right,
+  Text,
+  Badge,
+  View,
+  Content
+} from 'native-base';
+import HeaderSVG from '../component/icons/HeaderSVG';
+import {AdMobInterstitial, AdMobBanner} from 'expo-ads-admob'
 
 interface HomeProps {}
-const Home = (_props: HomeProps) => {
+class Home extends Component<HomeProps>{
+  BannerAdId: string;
+  AdMobInterstitialAdId: string;
+  constructor(props : Readonly<HomeProps>) {
+    super(props)
+    this.BannerAdId = Platform.OS === 'ios'
+                                    ? "ca-app-pub-5479742637870614/9709568594"
+                                    : "ca-app-pub-5479742637870614/8752366680"
+    this.AdMobInterstitialAdId = Platform.OS === 'ios'
+                                    ? "ca-app-pub-5479742637870614~9285869090"
+                                    : "ca-app-pub-5479742637870614~5673092993"
+  }
 
-  return (
-    <Container>
-      <Header transparent>
-        <Left>
-          <View>
-            <HeaderSVG />
+  async componentDidMount() {
+    AdMobInterstitial.setAdUnitID(this.AdMobInterstitialAdId);
+    await AdMobInterstitial.requestAdAsync({servePersonalizedAds: true});
+    await AdMobInterstitial.showAdAsync();
+    console.log(AdMobInterstitial)
+  }
+  render(){
+    return (
+      <Container>
+        <Header transparent>
+          <Left>
+            <View>
+              <HeaderSVG />
+            </View>
+          </Left>
+          <Right>
+            <Badge style={{ backgroundColor: 'lightblue', flexDirection: 'row'}}>
+              <Text style={{ color: 'white' }}>1866</Text>
+              <Text style={{ color: 'white' }}>Point</Text>
+            </Badge>
+          </Right>
+        </Header>
+        <Content>
+          <View style={styles.Bigcontainer}>
+            <AdMobBanner
+              bannerSize="fullBanner"
+              adUnitID={this.BannerAdId}
+              servePersonalizedAds={true}  
+            />
           </View>
-        </Left>
-        <Right>
-          <Badge style={{ backgroundColor: 'lightblue', flexDirection: 'row'}}>
-            <Text style={{ color: 'white' }}>1866</Text>
-            <Text style={{ color: 'white' }}>Point</Text>
-          </Badge>
-        </Right>
-      </Header>
-    </Container>
-  );
+        </Content>
+      </Container>
+    );
+  }
 };
 
 export default Home;
